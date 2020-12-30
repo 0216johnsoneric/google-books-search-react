481fd811f20ad7840db2e13ee1e730b0e435b721
Eric Johnson <ericjohnson0216@icloud.com>
2020-12-29 20:06:08 -0500

    first

---
+++ client/src/components/Main/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <div className="jumbotron jumbotron-fluid bg-info">
+            <div className="container">
+                <h1 className="display-2 text-center">(REACT) Google Books Search</h1>
+                <p className="lead text-center"><strong>Search for and Save Books of Interest</strong></p>
+            </div>
+        </div>
+    )
+}
+
+export default Main
